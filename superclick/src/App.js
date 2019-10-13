0f37c7b032b846d3230c8d5a497ff9e5cfaf7045
Ricardo Perdomo <hndgd1980@gmail.com>
2019-10-12 21:00:40 -0500

    images printing

--- superclick/src/App.js
+++ superclick/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import HeroCard from "./components/HeroCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import heroes from "./hero.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    heroes
+  };
+
+  removeHero = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const heroes = this.state.heroes.filter(hero => hero.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ heroes });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Heroes List</Title>
+        {this.state.heroes.map(hero => (
+          <HeroCard
+            removeHero={this.removeHero}
+            id={hero.id}
+            key={hero.id}
+            name={hero.name}
+            image={hero.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
