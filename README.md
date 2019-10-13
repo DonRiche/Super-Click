c34d38af90aa511b7ebb898f3ca75417cefb09b8
Ricardo Perdomo <hndgd1980@gmail.com>
2019-10-13 14:16:47 -0500

    movef folder

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Super-Click
-A memory game 
+## Super-Click
+
+<h1>How it works</h1>
+
+-The application renders different super hero images to the screen. Each image listen for click events.
+
+
+-The application keeps track of the user's score. The user's score increments when clicking an image for the first time.
+
+
+-The user's score reset to 0 if they click the same image more than once.
+
+
+-Every time an image is clicked, the images rendered to the page shuffle themselves in a random order.
+
+
+-Once the user's score is reset after an incorrect guess, the game restart.
--- src/App.js
+++ src/App.js
@@ -7,24 +7,32 @@ import Navbar from "./components/NavBar"
 class App extends Component {
   // Setting this.state.heroes to the heroes json array
   state = {
-    heroes
+    heroes,
+    count: 0
+  };
+
+  // handleIncrement increments this.state.count by 1
+  handleIncrement = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ count: this.state.count + 1 });
   };
 
   removeHero = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
+    // Filter this.state.heroes for heroes with an id not equal to the id being removed
     const heroes = this.state.heroes.filter(hero => hero.id !== id);
     // Set this.state.friends equal to the new friends array
     this.setState({ heroes });
   };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+  // Map over this.state.heroes and render a HeroCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Navbar></Navbar>
         {this.state.heroes.map(hero => (
           <HeroCard
-            removeHero={this.removeHero}
+            handleIncrement={this.handleIncrement}
+            // removeHero={this.removeHero}
             id={hero.id}
             key={hero.id}
             name={hero.name}
